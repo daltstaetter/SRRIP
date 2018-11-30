9ad3f25e4ddc71bd4e6177cdd9dc891fe3c07388
Dalton Altstaetter <daltstaetter@gmail.com>
2018-11-30 05:56:08 -0600

    Add LIRS scripts
    Signed-off-by: Dalton Altstaetter <daltstaetter@gmail.com>

---
+++ casim/zsim/run_script_PARSEC_LIRS.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "./hw4runscript PARSEC blackscholes LIRS"
+./hw4runscript PARSEC blackscholes LIRS
+
+echo "./hw4runscript PARSEC bodytrack LIRS"
+./hw4runscript PARSEC bodytrack LIRS
+
+echo "./hw4runscript PARSEC canneal LIRS"
+./hw4runscript PARSEC canneal LIRS
+
+echo "./hw4runscript PARSEC dedup LIRS"
+./hw4runscript PARSEC dedup LIRS
+
+echo "./hw4runscript PARSEC fluidanimate LIRS"
+./hw4runscript PARSEC fluidanimate LIRS
+
+echo "./hw4runscript PARSEC freqmine LIRS"
+./hw4runscript PARSEC freqmine LIRS
+
+echo "./hw4runscript PARSEC streamcluster LIRS"
+./hw4runscript PARSEC streamcluster LIRS
+
+echo "./hw4runscript PARSEC swaptions LIRS"
+./hw4runscript PARSEC swaptions LIRS
+
+echo "./hw4runscript PARSEC x264 LIRS"
+./hw4runscript PARSEC x264 LIRS
+
---
+++ casim/zsim/run_script_SPEC_LIRS.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "./hw4runscript SPEC bzip2 LIRS"
+./hw4runscript SPEC bzip2 LIRS
+
+echo "./hw4runscript SPEC gcc LIRS"
+./hw4runscript SPEC gcc LIRS
+
+echo "./hw4runscript SPEC mcf LIRS"
+./hw4runscript SPEC mcf LIRS
+
+echo "./hw4runscript SPEC hmmer LIRS"
+./hw4runscript SPEC hmmer LIRS
+
+echo "./hw4runscript SPEC sjeng LIRS"
+./hw4runscript SPEC sjeng LIRS
+
+echo "./hw4runscript SPEC libquantum LIRS"
+./hw4runscript SPEC libquantum LIRS
+
+echo "./hw4runscript SPEC xalan LIRS"
+./hw4runscript SPEC xalan LIRS
+
+echo "./hw4runscript SPEC milc LIRS"
+./hw4runscript SPEC milc LIRS
+
+echo "./hw4runscript SPEC cactusADM LIRS"
+./hw4runscript SPEC cactusADM LIRS
+
+echo "./hw4runscript SPEC leslie3d LIRS"
+./hw4runscript SPEC leslie3d LIRS
+
+echo "./hw4runscript SPEC namd LIRS"
+./hw4runscript SPEC namd LIRS
+
+echo "./hw4runscript SPEC soplex LIRS"
+./hw4runscript SPEC soplex LIRS
+
+echo "./hw4runscript SPEC calculix LIRS"
+./hw4runscript SPEC calculix LIRS
+
+echo "./hw4runscript SPEC lbm LIRS"
+./hw4runscript SPEC lbm LIRS
